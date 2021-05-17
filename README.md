0e059f9061861ca2822cdee05f833dd729e0d3e6
TanderBolt <79826346+TanderBolt@users.noreply.github.com>
2021-05-17 01:57:01 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# loader
+# TanderBolt
 
-Loader for Webpack WPP Web
+> TanderBolt is an open source project developed by the JavaScript community with the aim of exporting functions from Webpack modules
+
+## Usage
+```js
+var TanderBolt = require('@tanderbolt/loader');
+
+var loader = new TanderBolt();
+
+// Get module by id
+loader.get(moduleId);
+
+// Get module by search function
+loader.searchModule(m => m.default.MyFunctionTest);
+
+// Get module ID by search function
+loader.searchModuleId(m => m.default.MyFunctionTest);
+
+// Return a promise with resolved módule
+loader.waitForModule(m => m.default.MyFunctionTest);
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/*
+ * This file is part of TanderBolt.
+ *
+ * TanderBolt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TanderBolt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TanderBolt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    library: 'TanderBoltLoader',
+    // libraryTarget: 'var',
+    libraryTarget: 'umd',
+    // umdNamedDefine: true,
+    // libraryExport: 'default',
+    filename: 'loader.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
+
+  return config;
+};
